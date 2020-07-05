837ce6c196216d55f025839f404d3f8f5c3e81d6
杨尚书 <l>
2020-07-05 17:04:34 +0800

    Springboot整合mybatis

--- src/main/java/com/atguigu/test/GitTest.java
+++ src/main/java/com/atguigu/test/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("mmmmmmmmmmmmmmm");
+        System.out.println("2222222222222222");
     }
 }
